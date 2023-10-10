e71c8ce68007b9520826f1bae7cc9b3be8def729
GEORGE SENIOR MENSAH <euroclydon611@gmail.com>
2023-10-10 11:49:29 +0000

    fetch event added to service worker

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,7 +1,13 @@
-
 const cacheName = "v1";
 
-const cacheAssets = ["/", "/index.html", "/public/*", "/dist/*", "/src/*", "/assets/*" ];
+const cacheAssets = [
+  "/",
+  "/index.html",
+  "/public/*",
+  "/dist/*",
+  "/src/*",
+  "/assets/*",
+];
 
 // Install event
 self.addEventListener("install", (e) => {
@@ -30,6 +36,35 @@ self.addEventListener("activate", (e) => {
           }
         })
       );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      // Cache hit - return the response from the cache
+      if (response) {
+        return response;
+      }
+
+      // If the request is not in the cache, fetch it from the network
+      return fetch(e.request).then((response) => {
+        // Check if we received a valid response from the network
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // Clone the response to store it in the cache
+        const responseToCache = response.clone();
+
+        caches.open(cacheName).then((cache) => {
+          cache.put(e.request, responseToCache);
+        });
+
+        return response;
+      });
     })
   );
 });
